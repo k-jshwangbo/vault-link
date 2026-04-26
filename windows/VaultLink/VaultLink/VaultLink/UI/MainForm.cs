2861b65a354bceed5a35a0eaab98851f5d2c05ef
jshwangbo <jshwangbo3411@gmail.com>
2026-04-26 16:53:06 +0900

    chore: (windows) 프로젝트 솔루션 추가
    - MainForm

---
+++ windows/VaultLink/VaultLink/VaultLink/UI/MainForm.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace VaultLink.UI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+    } // end class
+}
